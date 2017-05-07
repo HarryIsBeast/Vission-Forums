d227436aa46ffc58c753cfcff8a79f065bb1cfd3
JackMcCally <jacksta414@gmail.com>
2017-05-07 13:04:38 -0500

    The base site has been made. NO CSS YET

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Vission - Forums</title>
+    <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="logo">
+          <h1>Vission Roleplay</h1>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="forums">Forums</a></li>
+            <li><a href="shop">Shop</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    
+    <section id="server-showcase">
+      <div class="container">
+        <h1>DarkRP</h1>
+        <p>We offer a DarkRP server with lots of custom features that improve the overall user experience!</p>
+      </div>
+    </section>
+    
+    <section id="login">
+      <h1>Login</h1>
+      <form action="login.php" type="POST">
+        Username: <input type="text"> <br>
+        Password: <input type="password"> <br>
+        <button type="submit">Login</button>
+      </form>
+      <form action="register.php">
+        
+      </form>
+    </section>
+  </body>
+</html>
